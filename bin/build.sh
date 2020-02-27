4db4e4da8132a7a32537a89cba2190af85922c39
Stefan Poensgen <mail@stefanpoensgen.de>
2020-02-27 13:21:02 +0100

    Updated build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-pluginName=${PWD##*/}
-
-commit=$1
-if [ -z ${commit} ]; then
-    commit=$(git tag --sort=-creatordate | head -1)
-    if [ -z ${commit} ]; then
-        commit="master";
-    fi
-fi
-
-# Remove old release
-rm -rf ${pluginName}-*.zip
-
-# Build new release
 npm install --prefix src/Resources/app/storefront
 cd ../../../
 ./psh.phar administration:build
 ./psh.phar storefront:build
-cd custom/plugins/${pluginName}
-git archive --prefix ${pluginName}/ ${commit} --format=zip > ${pluginName}-${commit}.zip
