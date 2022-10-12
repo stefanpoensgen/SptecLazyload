afd7252c8ed46fa627b03b626518e5cfa1dd80e5
Stefan Poensgen <mail@stefanpoensgen.de>
2022-10-12 13:04:24 +0200

    feat: update lazysizes

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -5,8 +5,15 @@ import 'lazysizes/plugins/attrchange/ls.attrchange';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 
-document.addEventListener('lazyloaded', Debouncer.debounce(function(event){
-    if(event.target.classList.contains('tns-complete')) {
-        window.dispatchEvent(new Event('resize'));
+document.addEventListener('lazybeforesizes', (e) => {
+    const aspectRatio = e.target.getAttribute('data-aspectratio');
+    if (!aspectRatio) {
+        return;
     }
-}, 400));
+
+    if (e.detail.width === parseFloat(aspectRatio)) {
+        e.detail.width = e.detail.instance.parentFit.getFit(e.target).parent.clientWidth;
+    }
+
+    e.detail.width = Math.round(e.detail.width);
+});
